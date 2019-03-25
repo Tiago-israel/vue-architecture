66a5be6edd0fc5b6033babde50a89d80e7ff6650
tiago <tiagoisrael77@hotmail.com>
2019-03-25 07:50:26 -0300

    ajuste

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,32 @@
       <div id="content">
         <top-bar></top-bar>
         <!-- Begin Page Content -->
-        <div class="container-fluid">
-          <div class="row">
-            <div v-for="(card,index) in projeto.cards" :key="index" class="col-12 col-sm-6 col-md-4">
-              <collapse-card :title="card.name" :idCard="`card${index}`">
-                <template slot="card-body">
-                  <container group-name="2" @drop="onDropTask(card, $event)" :get-child-payload="(i) => getChildPayload(i,card.tasks)">
-                    <draggable orientation="horizontal" v-for="(task,indexTask) in card.tasks" :key="indexTask">
-                      <div class="draggable-item">
-                        <card>
-                          <template slot="card-body">{{ task.descricao }}</template>
-                        </card>
-                      </div>
-                    </draggable>
-                  </container>
-                </template>
-              </collapse-card>
-            </div>
-          </div>
+        <div class="">
+            <Container orientation="horizontal" @drop="onColumnDrop($event)" :tag="{value: '',props:{class: ''}}">
+              <draggable v-for="card in projeto.cards" :key="card.id" :tag="{value: '',props:{class: 'col-md-4'}}">
+                <collapse-card :title="card.name" :idCard="`card${card.id}`">
+                  <template slot="card-body">
+                    <container
+                      group-name="2"
+                      @drop="onDropTask(card, $event)"
+                      :get-child-payload="(i) => getChildPayload(i,card.tasks)"
+                    >
+                      <draggable
+                        orientation="horizontal"
+                        v-for="(task,indexTask) in card.tasks"
+                        :key="indexTask"
+                      >
+                        <div>
+                          <card>
+                            <template slot="card-body">{{ task.descricao }}</template>
+                          </card>
+                        </div>
+                      </draggable>
+                    </container>
+                  </template>
+                </collapse-card>
+              </draggable>
+            </Container>
         </div>
         <!-- /.container-fluid -->
       </div>
@@ -58,12 +66,14 @@ export default {
     projeto: {}
   }),
   methods: {
-    onDropCard(projeto, dropResult){
+    onColumnDrop(dropResult) {
+      const projeto = Object.assign({}, this.projeto);
       projeto.cards = applyDrag(projeto.cards, dropResult);
+      this.projeto = projeto;
     },
     onDropTask(card, dropResult) {
       card.tasks = applyDrag(card.tasks, dropResult);
-      console.log(this.projeto)
+      console.log(this.projeto);
     },
     getChildPayload: (index, list) => list[index],
     buscarProjeto: async id => {
@@ -78,4 +88,7 @@ export default {
 </script>
 
 <style>
+  .smooth-dnd-container.horizontal{
+    display: flex !important;
+  }
 </style>
